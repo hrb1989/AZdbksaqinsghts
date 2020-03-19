08841eacdd7b8e1217f9ae6ba382668b431af35d
Hiran Ram Babu Ontivillu <37320572+hrb1989@users.noreply.github.com>
2020-03-20 00:55:38 +0530

    Commit

---
+++ notebooks/Users/hiranrambabu.ontivillu@outlook.com/2020-03-19 - Azure Blob Storage Import Example Notebook (2).py
@@ -0,0 +1,106 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC 
+# MAGIC This notebook shows you how to create and query a table or DataFrame loaded from data stored in Azure Blob storage.
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC 
+# MAGIC ### Step 1: Set the data location and type
+# MAGIC 
+# MAGIC There are two ways to access Azure Blob storage: account keys and shared access signatures (SAS).
+# MAGIC 
+# MAGIC To get started, we need to set the location and type of the file.
+
+# COMMAND ----------
+
+storage_account_name = "azdbksd7stg1"
+storage_account_access_key = "+D6GeGvd01IT559ngKZbmGzFWwXtRmXE+TyiAlwYtQ+whtGtMWrRF4VJpcDHooRx9LFCtshe6wbD+HoYzoGM5A=="
+
+# COMMAND ----------
+
+#file_location = "wasbs://example/location"
+#wasbs://<container Name>@<storage account name>.<stg type - blob>.core.windows.net/<blob path>
+#https://azdbksd7stg1.blob.core.windows.net/azdbksd7cont1/weekly-rent-paid-by-household-2018-census-csv.csv
+#DefaultEndpointsProtocol=https;AccountName=azdbksd7stg1;AccountKey=+D6GeGvd01IT559ngKZbmGzFWwXtRmXE+TyiAlwYtQ+whtGtMWrRF4VJpcDHooRx9LFCtshe6wbD+HoYzoGM5A==;EndpointSuffix=core.windows.net
+file_location = "wasbs://azdbksd7cont1@azdbksd7stg1.blob.core.windows.net/weekly-rent-paid-by-household-2018-census-csv.csv"
+file_type = "csv"
+
+# COMMAND ----------
+
+spark.conf.set(
+  "fs.azure.account.key."+storage_account_name+".blob.core.windows.net",
+  storage_account_access_key)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC 
+# MAGIC ### Step 2: Read the data
+# MAGIC 
+# MAGIC Now that we have specified our file metadata, we can create a DataFrame. Notice that we use an *option* to specify that we want to infer the schema from the file. We can also explicitly set this to a particular schema if we have one already.
+# MAGIC 
+# MAGIC First, let's create a DataFrame in Python.
+
+# COMMAND ----------
+
+df = spark.read.format(file_type).option("inferSchema", "true").option("header","true").load(file_location)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC 
+# MAGIC ### Step 3: Query the data
+# MAGIC 
+# MAGIC Now that we have created our DataFrame, we can query it. For instance, you can identify particular columns to select and display.
+
+# COMMAND ----------
+
+display(df.select("Code"))
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC 
+# MAGIC ### Step 4: (Optional) Create a view or table
+# MAGIC 
+# MAGIC If you want to query this data as a table, you can simply register it as a *view* or a table.
+
+# COMMAND ----------
+
+temp_table_name = "blobcsvtemp"
+
+
+df=df.withColumnRenamed("Weekly rent paid by household","week")
+df=df.withColumnRenamed("Households in rented occupied private dwellings", "household")
+
+df.createOrReplaceTempView(temp_table_name)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC 
+# MAGIC We can query this view using Spark SQL. For instance, we can perform a simple aggregation. Notice how we can use `%sql` to query the view from SQL.
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC 
+# MAGIC SELECT EXAMPLE_GROUP, SUM(EXAMPLE_AGG) FROM YOUR_TEMP_VIEW_NAME GROUP BY EXAMPLE_GROUP
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC 
+# MAGIC Since this table is registered as a temp view, it will be available only to this notebook. If you'd like other users to be able to query this table, you can also create a table from the DataFrame.
+
+# COMMAND ----------
+
+df.write.format("parquet").saveAsTable("blobcsv")
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC 
+# MAGIC This table will persist across cluster restarts and allow various users across different notebooks to query this data.
